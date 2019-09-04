8fe923f7e9715412e4044c46fe05c05c11ad2c33
Danielle <daniellernall@gmail.com>
2019-09-04 14:18:56 +0100

    Tidied up main page

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Danielle Nall</h1>
-      </header>
+    <div className="wrapper">
+      
       <Navigation>
         <h2>Danielle Nall</h2>
         <p>Hello 👋</p>
         <p>You've reached my portfolio, I'm a web developer from the North West of England</p>
         Link
       </Navigation>
+      <div className="main">
+      <header className="App-header">
+        <h1>Danielle Nall</h1>
+      </header>
+        </div>
     </div>
   );
 }
