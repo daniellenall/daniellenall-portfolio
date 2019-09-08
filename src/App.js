508d18046d2cdc9972f218c46f866c130eb6db55
Danielle <daniellernall@gmail.com>
2019-09-08 21:54:06 +0100

    Additional styling

--- src/App.js
+++ src/App.js
@@ -23,16 +23,24 @@ function App() {
         <header className="App-header">
 
         </header>
+        {/*<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+          <path fill="#810073" fill-opacity="1" d="M0,64L60,96C120,128,240,192,360,208C480,224,600,192,720,154.7C840,117,960,75,1080,69.3C1200,64,1320,96,1380,112L1440,128L1440,0L1380,0C1320,0,1200,0,1080,0C960,0,840,0,720,0C600,0,480,0,360,0C240,0,120,0,60,0L0,0Z"></path>
+  </svg>*/}
+
         <section id="about" className="section">
           <h2>About</h2>
           <p>Interested in computers from a young age, I have been inseparable from my laptop</p>
           <p>I like building the pretty parts of the internet, also known as being a front end developer.</p>
-          <p>Ex sit ullamco proident nisi ut nulla. Laboris mollit duis elit laboris magna consectetur fugiat dolor labore eu. Cillum veniam est nulla eiusmod qui nisi commodo aliquip Lorem consectetur. Nulla enim magna labore nulla laboris consectetur do duis mollit. Pariatur elit ea ipsum ad id id in est laboris. Ut non aute minim eu proident culpa laboris labore do voluptate.</p>
+          <p>You can keep up with me on <a href="http://codepen.io/daniellenall">CodePen</a>, <a href="http://github.com/daniellenall">GitHub</a>, or <a href="http://twitter.com/dncodes">Twitter</a></p>
         </section>
 
         <section id="projects" className="section">
           <h2>Projects</h2>
           <div className="project grid">
+            <div className="project">
+              <h3>Dev Resources</h3>
+              <p>My curation of sites I find useful when developing sites</p>
+            </div>
             <div className="project">
               <h3>100 Days of UI</h3>
               <p>Currently following </p>
@@ -57,13 +65,13 @@ function App() {
           <form action="https://formspree.io/hi@daniellenall.com" method="POST">
             <input type="text" name="name" />
             <input type="email" name="_replyto" />
-            <input type="submit" value="Send" />
+            <button type="submit" className="btn" value="Send">Send</button>
           </form>
 
         </section>
 
         <footer className="footer">
-            <p>Portfolio built by me using React, you can view the source on <a href="https://github.com/daniellenall/daniellenall">GitHub</a></p>
+          <p>Portfolio built by me using React, you can view the source on <a href="https://github.com/daniellenall/daniellenall">GitHub</a></p>
         </footer>
       </div>
     </div>
