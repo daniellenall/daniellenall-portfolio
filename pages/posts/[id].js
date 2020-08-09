888a6ea5241333ed9caffc551c875bb344ac9478
Danielle <daniellernall@gmail.com>
2020-08-09 21:15:38 +0100

    Add page template

---
+++ pages/posts/[id].js
@@ -0,0 +1,12 @@
+export default function Post({  }) {
+    return (
+        <Layout>
+            <Head>
+                <title></title>
+            </Head>
+            <article>
+                post
+            </article>
+        </Layout>
+    )
+}
