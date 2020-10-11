19e214084d1e637998abe85ea31e7a6c0f0ea72b
daniellenall <daniellernall@gmail.com>
2020-10-11 03:32:58 +0100

    Add up to date information to about

--- src/App.js
+++ src/App.js
@@ -31,13 +31,15 @@ function App() {
 
         <section id="about" className="section">
           <h2>About</h2>
-          <p>I'm a web developer from a small town in North Western England.</p>
+          <p>I'm a web developer from a small northern town in the UK.</p>
 
-          <p>I'd describe my self as a front end focused, full stack developer. Meaning I'm most comfortable with front end technologies such as HTML, CSS, JavaScript, and React. However, I can also happily work on back end code, having experience with Python and Django, as well as node.js</p>
+          <p>I'd describe my self as a front end focused, full stack developer. Meaning I'm most comfortable with front end technologies and frameworks such as HTML, CSS, SCSS, Bootstrap, Tailwind, JavaScript, and React. I'm also comfortable with coding in Python and Django, as well as node.js</p>
           
-          <p>To keep up to date with my work, you can check out <a href="http://codepen.io/daniellenall">CodePen</a> or <a href="http://github.com/daniellenall">GitHub</a>. I also ocassionally post on <a href="http://twitter.com/dncodes">Twitter</a>. I'm trying to constantly learn and develop my skills further</p>
-
-          <p>I'm also available for work, if you have something you'd like to discuss, <a href="mailto:hi@daniellenall.com">let's chat</a></p>
+          <p>To keep up to date with my work, you can check out <a href="http://codepen.io/daniellenall">CodePen</a> or <a href="http://github.com/daniellenall">GitHub</a>. I also ocassionally post on <a href="http://twitter.com/daniellernall">Twitter</a>. I'm trying to constantly learn and develop my skills further</p>
+          
+          <p>Improving this portfolio is on my list of todos, but for now I'd be happy to provide links to my recent work on request.</p>
+    
+          <p>I'm currently available for work, if you have something you'd like to discuss, <a href="mailto:hi@daniellenall.com">let's chat</a>.</p>
         </section>
         <ProjectGrid>
           <Project
