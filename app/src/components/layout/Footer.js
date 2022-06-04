44f0dbc4140632e9d41f271424554bfee9042c93
daniellenall <daniellernall@gmail.com>
2022-06-03 22:39:31 -0400

    Dynamic date

--- app/src/components/layout/Footer.js
+++ app/src/components/layout/Footer.js
@@ -2,10 +2,17 @@ import React, { Component } from 'react';
 import './Footer.css';
 
 class Footer extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          currentYear: new Date().getFullYear()
+        }
+    }
+
     render() {
         return (
             <footer className="footer">
-                <p>Portfolio built using React, you can view the source on <a href="https://github.com/daniellenall/daniellenall">GitHub</a></p>
+                <p>&copy; Danielle Nall  { this.state.currentYear } <a href="https://github.com/daniellenall/daniellenall">GitHub</a></p>
             </footer>
         );
     }
--- app/src/components/layout/Navigation.js
+++ app/src/components/layout/Navigation.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+
 import './Navigation.css';
 class Navigation extends Component {
     render() {
